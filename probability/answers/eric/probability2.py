305f32ae6e1396f8f7701d88f6695859f1c454ac
Eric Clack <eric@bn7.net>
2018-11-19 11:21:43 +0000

    Task 2

---
+++ probability/answers/eric/probability2.py
@@ -0,0 +1,17 @@
+# Probability 2: Python model answer by Eric
+
+import random
+
+print("Looking for pairs with two dice.")
+experiments = int(input("How many rolls of the dice? "))
+successes = 0
+
+def dice_roll(): return random.choice([1,2,3,4,5,6])
+
+for i in range(experiments):
+    a = dice_roll()
+    b = dice_roll()
+    if a == b:
+        successes += 1
+
+print("After %s experiments, probability of event is %s" % (experiments, successes/experiments))
